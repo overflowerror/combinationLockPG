40072c503b653fe779028136809a0014c87f528e
moorgar11 <clara1@aon.at>
2014-03-17 22:24:08 +0100

    works somehow

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var onSuccess = function(heading) {
     var element = document.getElementById('heading');
-    element.innerHTML = 'Heading: ' + heading;
+    element.innerHTML = 'Heading: ' + heading.magneticHeading;
+    drawDial(heading.magneticHeading*Math.PI/180);
 };
 
 var onErrors = function() {
-    alert('onError!');
+    alert('Compass error: ' + compassError.code);
 };
 
 var startup = function(){
-	var watchID = navigator.compass.getCurrentHeading(onSuccess, onErrors);
+ 	watchID = navigator.compass.watchHeading(onSuccess, onErrors, options);
 	lockinit();
 }
-window.onload = startup;
+
+var watchID = null;
+var options = { frequency: 1000 };
+
+document.addEventListener("deviceready", startup, false);
