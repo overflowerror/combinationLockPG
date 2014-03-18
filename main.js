1631d2ac2b6e5eec67b17ab6db803333142eba98
moorgar11 <clara1@aon.at>
2014-03-18 10:38:50 +0100

    works different again, ut works how it should

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
+    /*alert('Acceleration X: ' + acceleration.x + '\n' +
           'Acceleration Y: ' + acceleration.y + '\n' +
           'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
+          'Timestamp: '      + acceleration.timestamp + '\n' +
+		  ((Math.atan(acceleration.y / acceleration.x))/Math.PI*180)+90
+		);*/
+
+	var element = document.getElementById('textfeld');
+    element.innerHTML = "x:"+  acceleration.x + "y:"+  acceleration.y +" <br />"+ "<br />"+ "<br />"+getAngle(acceleration.x, acceleration.y);
+	drawDial(-getAngle(acceleration.x, acceleration.y)* Math.PI/180) ;
 };
+function getAngle (x, y) {
+ 		if (x == 0)
+			x += 0.00001;
+		x = -x;
+		if (x < 0) 
+			return (360 - Math.atan(y / x) * 360 / 2 / Math.PI - 90);
+		else 
+			return -(Math.atan(y / x) * 360 / 2 / Math.PI - 90);
+}
 
 function onError() {
     alert('onError!');
 };
 
 var startup = function(){
- 	watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError);
+ 	watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError,options);
 	lockinit();
 }
 
 
-var options = { frequency: 1000 };
+var options = { frequency: 100 };
 
 document.addEventListener("deviceready", startup, false);
